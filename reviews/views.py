eb62954cac2642c0dbf06ea88e4753a43acabece
alex-colvin <alexcolvin@live.com>
2022-04-14 11:11:58 -0400

    Created Put and Delete functionalities for Reviews

--- reviews/views.py
+++ reviews/views.py
@@ -6,7 +6,7 @@ from .serializers import ReviewSerializer
 from .models import Review
 from products.models import Product
 
-@api_view(['GET'])
+@api_view(['GET',])
 def reviews_list(request):
     
     if request.method == 'GET':
@@ -16,14 +16,14 @@ def reviews_list(request):
 
 @api_view(['GET','POST','PUT','DELETE'])
 def review_detail(request,pk):
-    request.data.update({"product": pk})
-
+    review = get_object_or_404(Review, pk=pk)
     if request.method == 'GET':
         reviews = Review.objects.filter(product_id=pk)
         serializer = ReviewSerializer(reviews, many=True)
         return Response (serializer.data, status=200)
 
     elif request.method == 'POST':
+        request.data.update({"product": pk})
         serializer = ReviewSerializer(data=request.data)
         if serializer.is_valid() == True:
             serializer.save()
@@ -31,12 +31,13 @@ def review_detail(request,pk):
         else:
             return Response(serializer.data.product, status=400)
 
-    # elif request.method == 'PUT':
-    #     serializer = ReviewSerializer(review, data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(serializer.data, status=201)
-    # elif request.method == 'DELETE':
-    #     review.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'PUT':
+        serializer = ReviewSerializer(review, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=201)
+
+    elif request.method == 'DELETE':
+        review.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
